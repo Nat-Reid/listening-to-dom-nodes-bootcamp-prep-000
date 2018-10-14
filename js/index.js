b96ca9581e459333050b45f825b6fac24c723d2f
Nat Reid <nathanielreid@gmail.com>
2018-10-14 23:26:48 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,27 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+function bubbleStopAt3(e) {
+  // stop! that! propagation!
+  var value = this.firstChild.nodeValue.trim();
+  if (value === '3'){
+    e.preventDefault();
+  }
+  console.log(value+ ' bubbled');
+}
+
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+const divs = document.querySelectorAll('div');
+for (let i = 0; i < divs.length; i++) {
+  //divs[i].addEventListener('click', bubble);
+  divs[i].addEventListener('click', bubbleStopAt3);
+}
+
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    return e.preventDefault()
+  } else {
+    console.log(e.which)
+  }
+});
